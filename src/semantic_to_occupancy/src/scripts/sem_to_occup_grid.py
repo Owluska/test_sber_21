4608dd1ca0649840e3e16cde7936a4c4c3b4c7cf
Owluska <kagirina@gmail.com>
2021-09-04 17:40:23 +0300

    Parsering continue

--- src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
+++ src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
@@ -8,18 +8,16 @@ import pandas as pd
 path = "/home/kseniia/downloads/semantic_pseudo_point_cloud.bag"
 
 bag = rosbag.Bag(path)
-df = pd.DataFrame(columns = ["x", "y", "z", "color"])
+
 topic = "/stereo_depth/point_cloud"
-#print(msg.fields, list(msg.data))
-row = {"x":0.0, "y":0.0, "z":0.0, "color":0.0}
-
-
 #all fields has FLOAT32 datatype
 d_bytes = 4
-#amounts of fields
-fields = 4
-loc = 0
+
+it = 0
 for topic, msg, t in bag.read_messages(topics = topic):
+    it += 1
+
+    fields = len(msg.fields)
     l = len(msg.data)
     if msg.is_bigendian:
         control_str = ">"
@@ -31,22 +29,20 @@ for topic, msg, t in bag.read_messages(topics = topic):
     data = bytes(msg.data)
     data = struct.unpack(control_str, data)
     
-    offset = int(msg.fields[3].offset/d_bytes)
-    #df = []
+    offset = int(msg.fields[3].offset/d_bytes) + 1
+    #print(data)
+    df = []
  
     for i, d in enumerate(data):
         if i % (offset) == 0 and i != 0 and i != (len(data) - 1):
-            j = i - offset
-
+            j = i - offset 
             #offset for x, y, z, color data
-            xo, yo, zo, co = [int(msg.fields[i].offset/d_bytes) for i in range(fields)]
-
-            row["x"] = data[j + xo]
-            row["y"] = data[j + yo]
-            row["z"] = data[j + zo]
-            row["color"] = data[j + co]
-            df.append(row, ignore_index=True)
-    break
+            row = [data[j + int(msg.fields[i].offset/d_bytes)] for i in range(fields)]
+            df.append(row)
+    print(it, end = " ")
+    if it > 100:
+        break
 
 bag.close()
-print(df.head())
+df = pd.DataFrame(df, columns = ['x', 'y', 'z','color'])
+print(df.head(),"\n", df.shape)
