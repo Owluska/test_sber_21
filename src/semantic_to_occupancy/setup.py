919bf9082072f1dd4c914a2ea7de3378c5554b90
Owluska <kagirina@gmail.com>
2021-09-04 20:13:27 +0300

    Sub-pub version

--- src/semantic_to_occupancy/setup.py
+++ src/semantic_to_occupancy/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages = ['src.scripts',  'src.scripts.data'],
     package_dir = {'':'src'},
-    requires = ['roslib', 'rospy', 'std_msgs']
+    requires = ['roslib', 'rospy', 'std_msgs', 'sensor_msgs', 'nav_msgs']
 )
 
 setup(**setup_args)
--- src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
+++ src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
@@ -1,86 +1,71 @@
-from re import L
 #! /usr/bin/env python3
-import rosbag
-import sensor_msgs.point_cloud2 as pc2
+import rospy
+from sensor_msgs.msg import PointCloud2
+from nav_msgs.msg import OccupancyGrid 
 import struct
 import numpy as np
-import pandas as pd
-path = "/home/kseniia/downloads/semantic_pseudo_point_cloud.bag"
+# import rosbag
+# path = "/home/kseniia/downloads/semantic_pseudo_point_cloud.bag"
+# bag = rosbag.Bag(path)
 
-bag = rosbag.Bag(path)
+# topic = "/stereo_depth/point_cloud"
+# for topic, msg, t in bag.read_messages(topics = topic):
+#     data = unpack_points(msg)
+#     cmap  = points_to_costmap(msg, classes, costs)
 
-topic = "/stereo_depth/point_cloud"
-#all fields has FLOAT32 datatype
-d_bytes = 4
+# bag.close()
 
-it = 0
+class cloud_to_costmap():
 
-# fence - изгородь, pole - столб, vegetation - растительность
-# terrain - местность
-keys = ['road', 'sidewalk', 'building', 'wall','fence',
-        'pole', 'traffic light', 'traffic sign', 'vegetation',
-        'terrain', 'sky', 'person', 'rider', 'car', 'truck',
-        'bus', 'train', 'motorcycle', 'bicycle']
-classes = {k:i for k, i in zip(keys, range(len(keys)))}
-obstacles = ['building', 'wall','fence','pole', 'traffic sign', 
-             'person', 'rider', 'car', 'truck',
-             'bus', 'train', 'motorcycle', 'bicycle']
+    # fence - изгородь, pole - столб, vegetation - растительность
+    # terrain - местность
+    keys = [         'road',     'sidewalk',   'building',   'wall', 'fence',       'pole',
+            'traffic light', 'traffic sign', 'vegetation','terrain',   'sky',     'person',
+                    'rider',          'car',      'truck',    'bus', 'train', 'motorcycle', 
+                'bicycle']
+    costs = [50,   0, 100, 100, 100, 100,
+            50, 100,  75,  75,   0, 100,
+            100, 100, 100, 100, 100, 100,
+            100]              
+    classes = {k:i for k, i in zip(keys, range(len(keys)))}
+    sub_name = '/stereo_depth/point_cloud'
+    def __init__(self):
+        self.pub = rospy.Publisher('costmap', OccupancyGrid, queue_size=10)
+        self.sub = rospy.Subscriber(self.sub_name, PointCloud2, queue_size=10)
 
-free = ['sidewalk', 'sky']
-fifty = ['road', 'traffic light']
-seventy_five = ['vegetation', 'terrain']
-#print(classes)
-for topic, msg, t in bag.read_messages(topics = topic):
-    it += 1
+    def unpack_points(self, msg):
+        fields = len(msg.fields)
+        l = len(msg.data)
+        if msg.is_bigendian:
+            control_str = ">"
+        else:
+            control_str = "<"
+        for i in range(int(l/fields)):
+            control_str += "f"
+        data = struct.unpack(control_str, bytes(msg.data))
+        return data
 
-    fields = len(msg.fields)
-    l = len(msg.data)
-    if msg.is_bigendian:
-        control_str = ">"
-    else:
-        control_str = "<"
-    for i in range(int(l/fields)):
-        control_str += "f"
-    
-    data = bytes(msg.data)
-    data = struct.unpack(control_str, data)
-    
-    #offset = int(msg.fields[3].offset/d_bytes) + 1
-    #print(data)
-    #df = []
+    def points_to_costmap(self, msg):
+        data = self.unpack_points(msg)
+        fields = len(msg.fields)
+        da = np.array(data).reshape((int(len(data)/fields), fields))
+        da.sort(axis = 0)
+        cost_map = [self.costs[int(c)] if c != -1 else -1 for c in da[:, 3]]
+        return cost_map
 
-    #npa = np.zeros((int(len(data)/fields), fields))
-    npa = np.array(data).reshape((int(len(data)/fields), fields))
-    #print(npa.shape)
-    # for i, d in enumerate(data):
-    #     if i % (offset) == 0 and i != 0 and i != (len(data) - 1):
-    #         j = i - offset 
-    #         #offset for x, y, z, color data
-    #         row = [data[j + int(msg.fields[i].offset/d_bytes)] for i in range(fields)]
-    #         df.append(row)
-    npa.sort(axis = 0)
-    map = []
-    for r in npa:
-        for k, e in classes.items():
-            color = int(r[3])
-            if e == color:
-                #print(e, color, k)
-                if k in free:
-                    map.append(0)
-                elif k in obstacles:
-                    map.append(100)
-                elif k in fifty:
-                    map.append(50)
-                elif k in seventy_five:
-                    map.append(75)
-                else:
-                    map.append(-1)
-    if it > 1:
-        print(map[:100])
-        print(npa[:100])
-        break
+    def callback(self, msg):
+        self.pub.data = self.points_to_costmap(msg)
+        self.pub.publish()
+        
+    def node(self):
+        rospy.spin()
 
-bag.close()
-# print()
-# df = pd.DataFrame(df, columns = ['x', 'y', 'z','color'])
-# print(df.head())
+
+
+if __name__ == '__main__':
+    rospy.init_node('cloud_to_costmap_node', anonymous=True)
+    c2m = cloud_to_costmap()
+    try:
+        c2m.node()
+    except rospy.ROSInterruptException:
+        pass
