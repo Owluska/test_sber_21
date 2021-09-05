915a7234b784fa0e1b4e66e587be1c2501cb5c2c
Owluska <kagirina@gmail.com>
2021-09-05 20:00:14 +0300

    continue cpp

--- src/sem_to_costmap_cpp/src/sem_to_costmap.cpp
+++ src/sem_to_costmap_cpp/src/sem_to_costmap.cpp
@@ -1,6 +1,36 @@
 #include "ros/ros.h"
 #include "rosbag/bag.h"
-#include "std_msgs/Int32.h"
-#include "std_msgs/String.h"
+#include "rosbag/view.h"
 
-const char path[100] = "/home/kseniia/downloads/semantic_pseudo_point_cloud.bag";
+#include "sensor_msgs/PointCloud2.h"
+#include "nav_msgs/OccupancyGrid.h"
+
+#include <boost/foreach.hpp>
+
+
+// const char path[100] = "/home/kseniia/downloads/semantic_pseudo_point_cloud.bag";
+rosbag::Bag bag;
+std::string path, topic;
+
+int main(int argc, char **argv)
+{
+    path = std::string("/home/kseniia/downloads/semantic_pseudo_point_cloud.bag");
+    topic = std::string("/stereo_depth/point_cloud");
+    bag.open(path, rosbag::bagmode::Read);
+    //rosbag::View view(bag, rosbag::TopicQuery(topic));
+    
+    for(rosbag::MessageInstance const m : rosbag::View(bag, rosbag::TopicQuery(topic)))
+    {
+        m.getTopic();
+        sensor_msgs::PointCloud2::ConstPtr msg_p = m.instantiate<sensor_msgs::PointCloud2>();
+        if(msg_p != nullptr)
+        {
+            std::cout << msg_p->data << std::endl;
+        }
+        return;
+    }
+    bag.close();
+}
+
+
+
