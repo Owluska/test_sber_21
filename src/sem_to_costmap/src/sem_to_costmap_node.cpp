583f6b1ecfd9f5a1ca7d488ab2179325fe101cf0
Owluska <kagirina@gmail.com>
2021-09-06 17:29:30 +0300

    Started data parsing debugging

--- src/sem_to_costmap/src/sem_to_costmap_node.cpp
+++ src/sem_to_costmap/src/sem_to_costmap_node.cpp
@@ -6,6 +6,7 @@ class points_to_map{
     public:
         nav_msgs::OccupancyGrid map_msg;
         sensor_msgs::PointCloud2 points_msg;
+        bool ifGot = false;
         void myCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
 
 };
@@ -14,18 +15,20 @@ void points_to_map::myCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
     //TODO check if this right 
     this->points_msg = *msg;
+    this->ifGot = true;
 }
 
 int main(int argc, char **argv)
 {
     std::string topic = std::string("/stereo_depth/point_cloud");
-    // nav_msgs::OccupancyGrid map_msg;
-    // sensor_msgs::PointCloud2 points_msg;
-    ros::NodeHandle n;
+    //ROS_INFO("Starting node..");
+
     points_to_map ptmObject;
     
     
     ros::init(argc, argv, "sem_to_costmap");
+    ros::NodeHandle n;
+
 
     ptmObject.map_msg.info.origin.position.x = 0;
     ptmObject.map_msg.info.origin.position.y = 0;
@@ -37,40 +40,45 @@ int main(int argc, char **argv)
     ptmObject.map_msg.info.origin.orientation.w = 1;
 
     ptmObject.map_msg.header.frame_id = "map";
-
+    
     ros::Publisher pub = n.advertise<nav_msgs::OccupancyGrid>("costmap", 1000);
     ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>(topic, 1000, &points_to_map::myCallback, &ptmObject);
 
     ros::Rate loop_rate(10);
-
+    //ROS_INFO("Starting loop..");
     while(ros::ok())
     {
-        std::vector<uint8_t> raw_data = ptmObject.points_msg.data;
-        
-        int bytes_count = ptmObject.points_msg.fields[0].offset;
-        //int data_length = sizeof(data)/sizeof(data[0]);
-        int data_length = raw_data.size();
-
-        std::vector<float> unpacked;
-        bool isBigEndian = ptmObject.points_msg.is_bigendian;
-        //for(int i, j = 0; i < data_length, j < data_length - 4; i++, j += 4)
-        for(auto i = raw_data.begin(); i != raw_data.end(); i++)
+        if (ptmObject.ifGot)
         {
-            uint32_t d = 0;
-            for(uint8_t j = 0; j < bytes_count; j++)
+            ptmObject.ifGot = false;
+            
+            
+            auto raw_data = ptmObject.points_msg.data;
+            int data_length = raw_data.size();
+            int bytes_count = ptmObject.points_msg.fields[1].offset;
+            std::vector<float> unpacked(data_length/4);
+            bool isBigEndian = ptmObject.points_msg.is_bigendian;
+            //for(int i, j = 0; i < data_length, j < data_length - 4; i++, j += 4)
+            for(auto i = raw_data.begin(); i != raw_data.end(); i++)
             {
-                if(!isBigEndian)
-                     d |= (*i << j); 
-                else
-                     d |= (*i >> j);         
-            }
-            std::cout << d;
-            unpacked.at(*i) = d;
-            i += bytes_count;
+                uint32_t d = 0;
+                for(uint8_t j = 0; j < bytes_count; j++)
+                {
+                    if(!isBigEndian)
+                        d |= (*i << j); 
+                    else
+                        d |= (*i >> j);         
+                }
+                unpacked.at(*i) = d;
+                ROS_INFO("%.2f,\t", unpacked.at(*i));
+                i += bytes_count;
+            }           
+            pub.publish(ptmObject.map_msg);
         }
-        
-        
-        pub.publish(ptmObject.map_msg);
+        else
+        {
+            ROS_INFO("Empty..");
+        }
         ros::spinOnce();
         loop_rate.sleep();
     }
