c1b7016e25f575cf74822d3c6d66e7f9673f770f
Owluska <kagirina@gmail.com>
2021-09-06 10:46:03 +0300

    map messge initialization added

--- src/sem_to_costmap_cpp/src/sem_to_costmap.cpp
+++ src/sem_to_costmap_cpp/src/sem_to_costmap.cpp
@@ -9,6 +9,7 @@ sensor_msgs::PointCloud2 points_msg;
 
 void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
+    //TODO check if this right 
     points_msg = *msg;
 }
 
@@ -20,15 +21,28 @@ int main(int argc, char **argv)
     
     ros::init(argc, argv, "sem_to_costmap");
 
+    map_msg.info.origin.position.x = 0;
+    map_msg.info.origin.position.y = 0;
+    map_msg.info.origin.position.z = 0;
+
+    map_msg.info.origin.orientation.x = 0;
+    map_msg.info.origin.orientation.y = 0;
+    map_msg.info.origin.orientation.z = 0;
+    map_msg.info.origin.orientation.w = 1;
+
+    map_msg.header.frame_id = "map";
+    
     ros::Publisher pub = n.advertise<nav_msgs::OccupancyGrid>("costmap", 1000);
     ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>(topic, 1000, callback);
 
-    ros::Rate loop(10);
+    ros::Rate loop_rate(10);
 
     while(ros::ok())
     {
+        std::vector<uint8_t> data = points_msg.data;
         pub.publish(map_msg);
         ros::spinOnce();
+        loop_rate.sleep();
     }
     ros::spin();
     return 0;
