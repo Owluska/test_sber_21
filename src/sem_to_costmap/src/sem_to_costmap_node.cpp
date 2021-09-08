d6466bfe25e4af24881de2b9522e060d294353be
Owluska <kagirina@gmail.com>
2021-09-08 15:55:49 +0300

    started testing pcl conversion

--- src/sem_to_costmap/src/sem_to_costmap_node.cpp
+++ src/sem_to_costmap/src/sem_to_costmap_node.cpp
@@ -39,13 +39,15 @@ float unpack(std::vector<uint32_t> bytes, bool isBigEndian)
     return f;
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr conversion(const boost::shared_ptr<const sensor_msgs::PointCloud2>& msg_input)
+void conversion(sensor_msgs::PointCloud2 msg_input,
+pcl::PointCloud<pcl::PointXYZRGB> &pcl_cloud)
 {
+    //first convert from sensor_msgs to pcl_cpl2
     pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*msg_input,pcl_pc2);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
-    return temp_cloud;
+    pcl_conversions::toPCL(msg_input, pcl_pc2);
+    //then convert from pcl_pcl2 to 
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2, pcl_cloud);
 }
 void calc_surface_Normals(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
                           pcl::PointCloud<pcl::_Normal>::Ptr normals)
@@ -58,6 +60,24 @@ void calc_surface_Normals(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
     ne.compute(*normals);
 }
 
+void calcSize(double &xMax, double &yMax, double &xMin, double &yMin,
+                                      pcl::PointCloud<pcl::PointXYZ> cld)
+{
+    for(int p = 0; p < cld.size(); p++)
+    {
+        double x = cld.points[p].x;
+        double y = cld.points[p].y;
+        if(xMax < x)
+            xMax = x;
+        if(xMin > x)
+            xMin = x;
+        if(yMax < y)
+            yMax = y;
+        if(yMin > y)
+            yMin = y;
+    }
+}
+
 int main(int argc, char **argv)
 {
     std::string topic = std::string("/stereo_depth/point_cloud");
@@ -67,6 +87,9 @@ int main(int argc, char **argv)
     
     ros::init(argc, argv, "sem_to_costmap");
     ros::NodeHandle n;
+
+    double xMin,  yMin = 1000;
+    double xMax,  yMax = -1000;
 
 
     ptmObject.map_msg.info.origin.position.x = 0;
@@ -91,26 +114,30 @@ int main(int argc, char **argv)
         {
             ptmObject.ifGot = false;
             
-            auto raw_data = ptmObject.points_msg.data;
-            uint8_t bytes_count = ptmObject.points_msg.fields[1].offset;
-            std::vector<float> unpacked(raw_data.size()/4);
-            bool isBigEndian = ptmObject.points_msg.is_bigendian;
+            // auto raw_data = ptmObject.points_msg.data;
+            // uint8_t bytes_count = ptmObject.points_msg.fields[1].offset;
+            // std::vector<float> unpacked(raw_data.size()/4);
+            // bool isBigEndian = ptmObject.points_msg.is_bigendian;
 
-            for(int i = 0; i < raw_data.back(); i++)
-            {
-                std::vector<uint32_t> buf(4);
-                for(uint8_t j = 0; j < sizeof(float); j ++)
-                {
-                    buf[j] = raw_data[j+i];
-                }
-                float f = unpack(buf, isBigEndian);
+            // for(int i = 0; i < raw_data.back(); i++)
+            // {
+            //     std::vector<uint32_t> buf(4);
+            //     for(uint8_t j = 0; j < sizeof(float); j ++)
+            //     {
+            //         buf[j] = raw_data[j+i];
+            //     }
+            //     float f = unpack(buf, isBigEndian);
 
-                unpacked[i] = f;
-                ROS_INFO("%.2f",f);
-                i += bytes_count - 1;
-            }
+            //     unpacked[i] = f;
+            //     ROS_INFO("%.2f",f);
+            //     i += bytes_count - 1;
+            // }
+            // ROS_INFO("Got vector with l: %d", unpacked.back());
+            pcl::PointCloud<pcl::PointXYZRGB> cld;
+            conversion(ptmObject.points_msg, cld);
 
-            ROS_INFO("Got vector with l: %d", unpacked.back());
+
+            ROS_INFO("Got vector with l: %d", cld.size());
             //pub.publish(ptmObject.map_msg);
         }
         ros::spinOnce();
