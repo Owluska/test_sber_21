a2187cd717cbe183b07053d255d834672dd64cf0
Owluska <kagirina@gmail.com>
2021-09-12 10:30:34 +0300

    map filling logic for python script corrected

--- src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
+++ src/semantic_to_occupancy/src/scripts/sem_to_occup_grid.py
@@ -47,49 +47,86 @@ class cloud_to_costmap():
         self.msg.info.origin.orientation.z = 0
         self.msg.info.origin.orientation.w = 1
 
+        self.Got = False
+        self.cloud_msg = PointCloud2()
+
         self.msg.header.frame_id = "map"
 
         self.sub = rospy.Subscriber(self.sub_name, PointCloud2, self.callback, queue_size=10)
         
 
     def unpack_points(self, msg):
-        fields = len(msg.fields)
+        n_fields = len(msg.fields) 
         l = len(msg.data)
+ 
         if msg.is_bigendian:
             control_str = ">"
         else:
             control_str = "<"
-        for i in range(int(l/fields)):
+        for i in range(int(l/n_fields)):
             control_str += "f"
+
         data = struct.unpack(control_str, bytes(msg.data))
         return data
     
-    def get_meta_data(self, msg, data):
-        l = data.shape[0]
-        self.msg.info.width = int(math.sqrt(l))
-        self.msg.info.height = int(math.sqrt(l))
- 
-        self.msg.info.resolution = 0.002
+    def get_meta_data(self, data):
+        #l = data.shape[0]
+        x_max, y_max = np.max(data[:, :2], axis = 0)
+        x_min, y_min = np.min(data[:, :2], axis = 0)
+        resolution = 1.0
+
+        self.msg.info.width = int((x_max - x_min)/resolution)#int(math.sqrt(l))
+        self.msg.info.height = int((y_max - y_min)/resolution)#int(math.sqrt(l))
+        self.msg.info.resolution = resolution
+
+        self.msg.header.stamp = rospy.Time.now()
+
+        self.msg.info.origin.position.x = -self.msg.info.width/2
+        self.msg.info.origin.position.y = -self.msg.info.height/2
     
     def points_to_costmap(self, msg):
         data = self.unpack_points(msg)
 
         fields = len(msg.fields)
         da = np.array(data).reshape((int(len(data)/fields), fields))
+
         da.sort(axis = 0)
 
-        self.get_meta_data(msg, da)
-        cost_map = [self.costs[int(c)] if c != -1 else -1 for c in da[:, 3]]
+        self.get_meta_data(da)
+
+        x_min, y_min = np.min(da[:, :2], axis = 0)
+        cost_map = np.ones((self.msg.info.width * self.msg.info.height), dtype='int8') * 50
+        
+        for d in da:
+            x = d[0]
+            y = d[1]
+            c = d[3]
+
+            ix = int((x - x_min)/self.msg.info.resolution) - 1
+            iy = int((y - y_min)/self.msg.info.resolution) - 1
+            i = int(ix * iy + ix)
+            if cost_map[i] == 100 or cost_map[i] == 0:
+                continue
+            cost_map[i] = self.costs[int(c)]
+
+        cost_map = list(cost_map)
+        #cost_map = [self.costs[int(c)] if c != -1 else -1 for c in da[:, 3]]
         #rospy.loginfo("Data shape: {}, map shape: {}".format(da.shape, len(cost_map)))
         return cost_map
 
-    def callback(self, msg):
-        self.msg.data = self.points_to_costmap(msg)
-        self.msg.header.stamp = rospy.Time.now()
-        self.pub.publish(self.msg)
+    def callback(self, msg): 
+        self.Got = True
+        self.cloud_msg = msg
+    
         
     def node(self):
-        rospy.spin()
+        while(not rospy.is_shutdown()):
+            if self.Got:
+                self.Got = False
+                self.msg.data = self.points_to_costmap(self.cloud_msg)
+                self.pub.publish(self.msg)
+
+
 
 
 
